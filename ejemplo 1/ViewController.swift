d981c17a7c3c1f1d04b8e34ab41bcf5de73b2887
Armandocarrillo <ingcompuarmando@gmail.com>
2019-12-18 14:46:35 -0600

    Initial Commit

---
+++ ejemplo 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ejemplo 1
+//
+//  Created by Armando Carrillo on 18/12/19.
+//  Copyright © 2019 Armando Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
